c649d1056bda875eeb8f8057db2d173621d7dd14
danielphilippi <daniel2.philippi@gmail.com>
2021-05-06 12:06:29 +0100

    move sudoku helper functions to own module

---
+++ charles/sudoku_utils.py
@@ -0,0 +1,64 @@
+
+def get_box_indices(base=3):
+    """
+    :param base: length of inner box
+    :return: row wise list of box indices
+    """
+    # total length of outer box
+    side=base*base
+    box_idx = [None for _ in range(side)]
+    # flat index of length i*j
+    pos = 0
+    for i in range(base):
+        for j in range(base):
+            # define starting value for each box
+            start = i*base*side + j*base
+            # c: starting point of each row inside box
+            # r: add rows inside box
+            box_idx[pos] = [start + c*side + r for c in range(base) for r in range(base)]
+            pos += 1
+    return box_idx
+
+
+def get_row_indices(base):
+    side = base*base
+    return [list(range(i, i + side)) for i in np.arange(0, side*side, side)]
+
+
+def get_col_indices(base):
+    side = base*base
+    return [np.arange(i, side*side, side) for i in list(range(side))]
+
+
+def get_indices(base):
+    row_idx = get_row_indices(base)
+    col_idx = get_col_indices(base)
+    box_idx = get_box_indices(base)
+    return row_idx, col_idx, box_idx
+
+
+def count_duplicates(seq):
+    '''takes as argument a sequence and
+    returns the number of duplicate elements'''
+
+    counter = 0
+    seen = set()
+    for elm in seq:
+        if elm in seen:
+            counter += 1
+        else:
+            seen.add(elm)
+    return counter
+
+
+def get_pop_sepcs(option):
+    if option == 1:
+        replacement = False
+        valid_set = [i for _ in range(side) for i in range(1, side +1)]
+
+    elif option == 2:
+        replacement = True
+        valid_set = list(range(1,side+1))
+
+
+    return replacement, valid_set
--- sudoku.py
+++ sudoku.py
@@ -6,57 +6,7 @@ from charles.mutation import binary_mutation, swap_mutation
 from charles.crossover import single_point_co
 from random import random, sample, randint
 from operator import  attrgetter
-
-import numpy as np
-
-def get_box_indices(base=3):
-    """
-    :param base: length of inner box
-    :return: row wise list of box indices
-    """
-    # total length of outer box
-    side=base*base
-    box_idx = [None for _ in range(side)]
-    # flat index of length i*j
-    pos = 0
-    for i in range(base):
-        for j in range(base):
-            # define starting value for each box
-            start = i*base*side + j*base
-            # c: starting point of each row inside box
-            # r: add rows inside box
-            box_idx[pos] = [start + c*side + r for c in range(base) for r in range(base)]
-            pos += 1
-    return box_idx
-
-def get_row_indices(base):
-    side = base*base
-    return [list(range(i, i + side)) for i in np.arange(0, side*side, side)]
-
-def get_col_indices(base):
-    side = base*base
-    return [np.arange(i, side*side, side) for i in list(range(side))]
-
-
-def get_indices(base):
-    row_idx = get_row_indices(base)
-    col_idx = get_col_indices(base)
-    box_idx = get_box_indices(base)
-    return row_idx, col_idx, box_idx
-
-def count_duplicates(seq):
-    '''takes as argument a sequence and
-    returns the number of duplicate elements'''
-
-    counter = 0
-    seen = set()
-    for elm in seq:
-        if elm in seen:
-            counter += 1
-        else:
-            seen.add(elm)
-    return counter
-
+from charles.sudoku_utils import get_indices, count_duplicates
 
 puzzle = sudoku_data_generator.Sudoku(difficulty=3)
 puzzle_ref = puzzle.puzzle_flat
@@ -73,16 +23,18 @@ def evaluate(self):
     # count errors in cols
     for col in col_idx:
         n_error += count_duplicates([repres[c] for c in col])
-    # count errors in squares
+    # count errors in box
     for box in box_idx:
         n_error += count_duplicates([repres[b] for b in box])
 
     # penalize deviation from initial puzzle
-    for pos, v in enumerate(puzzle_ref):
-        if repres[pos] != v:
-            n_error += 9
+    if False:
+        for pos, v in enumerate(puzzle_ref):
+            if repres[pos] != v:
+                n_error += 9
 
     return n_error
+
 
 def get_neighbours(self):
     pass
@@ -92,24 +44,12 @@ def get_neighbours(self):
 Individual.evaluate = evaluate
 Individual.get_neighbours = get_neighbours
 
-def get_pop_sepcs(option):
-    if option == 1:
-        replacement = False
-        valid_set = [i for _ in range(side) for i in range(1, side +1)]
 
-    elif option == 2:
-        replacement = True
-        valid_set = list(range(1,side+1))
-
-
-    return replacement, valid_set
-
-replacement, valid_set = get_pop_sepcs(option=1)
 
 if __name__ == '__main__':
 
     pop = Population(
-        size=300, optim="min", sol_size=side*side, valid_set=valid_set, replacement=replacement)
+        size=300, optim="min", sol_size=side*side, valid_set=None, replacement=None)
 
     pop.evolve(
         gens=100,
