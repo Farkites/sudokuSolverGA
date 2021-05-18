d8d06a2e2053134cb6616190ffdc26096290c9c0
danielphilippi <daniel2.philippi@gmail.com>
2021-05-18 23:12:42 +0100

    hotfix: resolve np types

--- sudoku.py
+++ sudoku.py
@@ -23,15 +23,15 @@ import json
 config_grid = {
     'difficulty': [3],  # [3,2,1]
     'epochs': [30],
-    'pop_size': list(np.arange(100, 1000, 250)),
+    'pop_size': np.arange(100, 1000, 250).tolist(),
     'gens': [1000],
     'optim': ['min'],
     'representation': ['with_replacement', 'without_replacement', 'maintain_init_puzzle'],
     'selection': ['tournament', 'fps'], # [tournament, fps]
     'mutation': ['swap_mutation', 'inversion_mutation', 'swap_by_row_mutation'],
     'crossover': ['single_point_co', 'cycle_co', 'arithmetic_co', 'partially_match_co', 'cycle_by_row_co', 'partially_match_by_row_co'],
-    'co_p': list(np.arange(.7, 1.05, .15)),
-    'mu_p': list(np.arange(.05, 0.35, .05)),
+    'co_p': np.arange(.7, 1.05, .15).tolist(),
+    'mu_p': np.arange(.05, 0.35, .05).tolist(),
     'elitism': [True],
     'fitness_sharing': [False],
     'diversity_measure': [True],
@@ -41,15 +41,15 @@ config_grid = {
 config_grid = {
     'difficulty': [3],  # [3,2,1]
     'epochs': [30],
-    'pop_size': list(np.arange(100, 1000, 250)),
+    'pop_size': np.arange(100, 1000, 250).tolist(),
     'gens': [1000],
     'optim': ['min'],
     'representation': ['with_replacement', 'without_replacement', 'maintain_init_puzzle'],
     'selection': ['tournament', 'fps'], # [tournament, fps]
     'mutation': ['swap_mutation', 'inversion_mutation', 'swap_by_row_mutation'],
     'crossover': ['single_point_co', 'arithmetic_co', 'partially_match_co', 'partially_match_by_row_co'],
-    'co_p': list(np.arange(.7, 1.05, .15)),
-    'mu_p': list(np.arange(.05, 0.35, .05)),
+    'co_p': np.arange(.7, 1.05, .15).tolist(),
+    'mu_p': np.arange(.05, 0.35, .05).tolist(),
     'elitism': [True],
     'fitness_sharing': [False],
     'diversity_measure': [True],
