774a26dafaf9c0acf7ecca0150c31d3ea0ed2d61
danielphilippi <daniel2.philippi@gmail.com>
2021-05-18 19:52:31 +0100

    add min to fps

--- charles/selection.py
+++ charles/selection.py
@@ -1,5 +1,7 @@
 from random import uniform, sample
 from operator import attrgetter
+import numpy as np
+from numpy.random import choice
 
 def fps(population):
     """Fitness proportionate selection implementation.
@@ -23,10 +25,29 @@ def fps(population):
             if position > spin:
                 return individual
     elif population.optim == "min":
-        raise NotImplementedError
+        # code adapted from https://rocreguant.com/roulette-wheel-selection-python/2019/
+        # Computes the totality of the population fitness
+        population_fitness = sum([chromosome.fitness for chromosome in population])
+
+        # Computes for each individual the probability
+        individual_probabilities = [individual.fitness / population_fitness for individual in population]
+
+        # Making the probabilities for a minimization problem
+        individual_probabilities = 1 - np.array(individual_probabilities)
+        individual_probabilities = individual_probabilities / sum(individual_probabilities)
+
+        #
+        idx = list(range(len(population.individuals)))
+
+        # test
+        # check = sorted([(idx, p) for idx, p in enumerate(individual_probabilities)], key=lambda x: x[1], reverse=True)
+        # Selects one individual based on the computed probabilities
+        selected_idx = choice(idx, p=individual_probabilities)
+        return population[selected_idx]
 
     else:
         raise Exception("No optimiziation specified (min or max).")
+
 
 def tournament(population, size=20):
     # Select individuals based on tournament size
