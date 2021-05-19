d6858a87215fc10b318ba287df6058194b3ae6c1
danielphilippi <daniel2.philippi@gmail.com>
2021-05-19 14:56:26 +0100

    added to configs available

--- configs_available.py
+++ configs_available.py
@@ -36,7 +36,26 @@ config_grid_2nd_run = {
     'early_stopping_patience': [100]
 }
 
-config_grid_testing = {
+
+config_grid_test_grid = {
+    'difficulty': [3],  # [3,2,1]
+    'epochs': [2],
+    'pop_size': [30],
+    'gens': [1000],
+    'optim': ['min'],
+    'representation': ['with_replacement', 'without_replacement', 'maintain_init_puzzle'],
+    'selection': ['tournament'],# 'fps'], # [tournament, fps]
+    'mutation': ['swap_mutation', 'inversion_mutation', 'swap_by_row_mutation'],
+    'crossover': ['single_point_co', 'arithmetic_co', 'partially_match_co', 'partially_match_by_row_co'],
+    'co_p': [.9],
+    'mu_p': [.01],
+    'elitism': [True],
+    'fitness_sharing': [False],
+    'diversity_measure': [True],
+    'early_stopping_patience': [5]
+}
+
+config_grid_test_single = {
     'difficulty': [3],  # [3,2,1]
     'epochs': [3],
     'pop_size': [50],
@@ -45,7 +64,7 @@ config_grid_testing = {
     'representation': ['with_replacement'], # [with_replacement, without_replacement, maintain_init_puzzle]
     'selection': ['tournament'], # [tournament, fps]
     'mutation': ['swap_mutation'], # [swap_mutation, inversion_mutation, swap_by_row_mutation]
-    'crossover': ['cycle_co'], # [single_point_co, cycle_co, arithmetic_co, partially_match_co, cycle_by_row_co, partially_match_by_row_co]
+    'crossover': ['single_point_co'], # [single_point_co, cycle_co, arithmetic_co, partially_match_co, cycle_by_row_co, partially_match_by_row_co]
     'co_p': [.9],
     'mu_p': [.01],
     'elitism': [True],
